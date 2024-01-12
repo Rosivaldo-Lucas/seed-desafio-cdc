012abfbf5a2ee7f149872ae64cd05aa815851d44
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2024-01-12 15:51:21 -0300

    desafio-cdc: Adiciona validacao na camada de dominio na criacao de uma nova Categoria.

--- src/main/java/com/rosivaldolucas/desafiocdcdeveficiente/categoria/Categoria.java
+++ src/main/java/com/rosivaldolucas/desafiocdcdeveficiente/categoria/Categoria.java
@@ -1,6 +1,7 @@
 package com.rosivaldolucas.desafiocdcdeveficiente.categoria;
 
 import jakarta.persistence.*;
+import org.springframework.util.StringUtils;
 
 @Entity
 @Table(name = "CATEGORIA")
@@ -20,6 +21,10 @@ public class Categoria {
 
   public static Categoria criar(final String nome) {
     return new Categoria(nome);
+  }
+
+  private void validar() {
+    if (!StringUtils.hasText(this.nome)) throw new IllegalArgumentException("nome é obrigatório.");
   }
 
   public Long getId() {
